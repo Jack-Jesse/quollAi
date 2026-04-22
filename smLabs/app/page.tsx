0ad4cf2bf5c19445fade0cbdd6879f564b159460
Jack-Jesse <jack.jesse@my.jcu.edu.au>
2026-04-22 12:27:32 +1000

    feat: add liquid glass button components with framer-motion animations

---
+++ smLabs/app/page.tsx
@@ -0,0 +1,11 @@
+import { Demo } from "@/components/demo";
+
+export const dynamic = "force-dynamic";
+
+export default function Home() {
+  return (
+    <main>
+      <Demo />
+    </main>
+  );
+}
